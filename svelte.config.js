e3f219618e86e3b490eade3eb41f55c517311111
oninsan <kokoybaldofordawin@gmail.com>
2025-08-31 15:14:44 +0800

    Removed base

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'development' ? '' : '/DevPortfolio'
+			base: ''
 		},
 		prerender: {
 			crawl: true,
